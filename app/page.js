e840c94715250fc790678a270b664cc3cf641327
DeuzivanViana <deuzivanlimaviana@proton.me>
2025-03-22 20:30:59 -0300

    add list of recived transaction

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import { ArrowRight, SendHorizonal } from 'lucide-react';
+import { ArrowLeft, ArrowRight, SendHorizonal } from 'lucide-react';
 import { Layout } from './components/Layout'
 import { NavigationBar } from './components/NavigationBar';
 import { createClient } from './utils/supabase/server';
@@ -8,7 +8,8 @@ export default async function Page() {
 
   const { data: { user }} = await supabase.auth.getUser()
   const { data: wallet} = await supabase.from('wallet').select('*').eq('user_id', user.id).maybeSingle()
-  const { data: transaction } = await supabase.from('transaction').select('*').eq('from_id', wallet.id).order('created_at', {ascending: false}).limit(21)
+  const { data: sended_transaction } = await supabase.from('transaction').select('*').eq('from_id', wallet.id).order('created_at', {ascending: false}).limit(21)
+  const { data: recived_transaction } = await supabase.from('transaction').select('*').eq('to_id', wallet.id).order('created_at', {ascending: false}).limit(21)
 
   return (
     <Layout>
@@ -18,9 +19,16 @@ export default async function Page() {
       </section>
 
       <h2 className='p-4 text-zinc-700 underi'>Last transactions</h2>
-      { transaction.map((val, index) => {
+      { sended_transaction.map((val, index) => {
         return <div key={index} className={'flex items-center gap-2 p-4 border-zinc-700 border-b ' + (index === 0 ? 'border-t' : '')}>
           <ArrowRight size={18} className='text-zinc-500'/>
+          <span className='text-red-500 text-xs'>{ val.amount } DTP to </span>
+          <span className='text-zinc-500 text-xs'>{ val.to_id }</span>
+        </div>
+      })}
+      { recived_transaction.map((val, index) => {
+        return <div key={index} className={'flex items-center gap-2 p-4 border-zinc-700 border-b ' + (index === 0 ? 'border-t' : '')}>
+          <ArrowLeft size={18} className='text-zinc-500'/>
           <span className='text-lime-500 text-xs'>{ val.amount } DTP to </span>
           <span className='text-zinc-500 text-xs'>{ val.to_id }</span>
         </div>
